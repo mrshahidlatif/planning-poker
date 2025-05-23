703749a8bdf3064658ba718c20154839a8c946fb
Shahid Latif <shahid.latif@utopiamusic.com>
2025-05-24 00:10:08 +0200

    Bugfix: page not loading correctly on reload

--- web/composables/useWebSocket.ts
+++ web/composables/useWebSocket.ts
@@ -1,19 +1,17 @@
 import type { Socket } from "socket.io-client";
 import { io } from "socket.io-client";
 
-const config = useRuntimeConfig();
-
-if (!config.public.webSocketUrl) {
-  throw new Error("NUXT_PUBLIC_WEB_SOCKET_URL is not set");
-}
-
-const url = config.public.webSocketUrl;
-
 let socket: Socket;
 
 export const useWebSocket = () => {
   if (!socket) {
-    socket = io(url);
+    const config = useRuntimeConfig();
+
+    if (!config.public.webSocketUrl) {
+      throw new Error("NUXT_PUBLIC_WEB_SOCKET_URL is not set");
+    }
+
+    socket = io(config.public.webSocketUrl);
   }
 
   return socket;
