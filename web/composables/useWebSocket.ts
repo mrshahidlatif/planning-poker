a2038a6c87a2e0986e8b910f6452cbe410674931
Shahid Latif <shahid.latif@utopiamusic.com>
2025-05-23 20:36:17 +0200

    Move web socket server url to env variable

--- web/composables/useWebSocket.ts
+++ web/composables/useWebSocket.ts
@@ -1,8 +1,8 @@
 import type { Socket } from "socket.io-client";
 import { io } from "socket.io-client";
 
-const url = "https://planning-poker-server-b7iw.onrender.com";
-// const url = "http://localhost:3001";
+const url =
+  import.meta.env.VITE_WEB_SOCKET_SERVER_URL || "http://localhost:3001";
 
 let socket: Socket;
 
